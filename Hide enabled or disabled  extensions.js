f7741f3490ee1ed377c3ad770367b4d4006aa190
cpuuntery <57891661+cpuuntery@users.noreply.github.com>
2021-08-03 17:35:37 +0300

    Add files via upload

---
+++ Hide enabled or disabled  extensions.js
@@ -0,0 +1,17 @@
+#Hide enabled extensions
+
+let elm_list = document.querySelector("body extensions-manager").shadowRoot.querySelector("#items-list").shadowRoot.querySelector("div.items-container").querySelectorAll("extensions-item");
+for ( let i=0; i < elm_list.length; i++)
+{if (elm_list[i].shadowRoot.querySelector("div#card.enabled.dev-mode") !== null) {
+  elm_list[i].shadowRoot.querySelector("div#card.enabled.dev-mode").remove()
+}}
+
+
+
+#Hide disabled extensions
+
+let elm_list = document.querySelector("body extensions-manager").shadowRoot.querySelector("#items-list").shadowRoot.querySelector("div.items-container").querySelectorAll("extensions-item");
+for ( let i=0; i < elm_list.length; i++)
+{if (elm_list[i].shadowRoot.querySelector("div#card.disabled.dev-mode") !== null) {
+  elm_list[i].shadowRoot.querySelector("div#card.disabled.dev-mode").remove()
+}}
