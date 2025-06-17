094585e8fb165a7ed04f03a39453fe88fd244372
cpuuntery <57891661+cpuuntery@users.noreply.github.com>
2025-06-17 19:56:00 +0300

    Update Hide enabled or disabled  extensions.js

--- Hide enabled or disabled  extensions.js
+++ Hide enabled or disabled  extensions.js
@@ -15,3 +15,23 @@ for ( let i=0; i < elm_list.length; i++)
 {if (elm_list[i].shadowRoot.querySelector("div#card.disabled.dev-mode") !== null) {
   elm_list[i].shadowRoot.querySelector("div#card.disabled.dev-mode").remove()
 }}
+
+
+//improved version just remove the [shadowRoot.querySelector("div#card.enabled.dev-mode")]
+//tested to work with chromium 117
+
+//Hide enabled extensions
+let elm_list = document.querySelector("body > extensions-manager").shadowRoot.querySelector("#items-list").shadowRoot.querySelector("#content-wrapper > div:nth-child(3)").querySelectorAll("extensions-item");
+for ( let i=0; i < elm_list.length; i++)
+{if (elm_list[i].shadowRoot.querySelector("div#card.enabled.dev-mode") !== null) {
+  elm_list[i].remove()
+}}
+
+
+//Hide disabled extensions
+let elm_list = document.querySelector("body > extensions-manager").shadowRoot.querySelector("#items-list").shadowRoot.querySelector("#content-wrapper > div:nth-child(3)").querySelectorAll("extensions-item");
+for ( let i=0; i < elm_list.length; i++)
+{if (elm_list[i].shadowRoot.querySelector("div#card.disabled.dev-mode") !== null) {
+  elm_list[i].remove()
+}}
+
